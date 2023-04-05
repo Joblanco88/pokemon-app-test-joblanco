1cad98fe8767e87e0343bcd50be1ced0d0714f44
Joblanco88 <jo_blanco9@yahoo.com.br>
2023-04-05 16:58:50 -0300

    Criado renderização das imagens dos pokémon e botão de favorito

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { fetchAllPokemon } from '../helpers/api';
+import { fetchAllPokemon, fetchPokemon } from '../helpers/api';
+import blackHeart from '../images/blackHeartIcon.svg';
 
 export default function List() {
   const [pokemonList, setPokemonList] = useState([]);
+  const favoriteIcon = blackHeart;
 
   useEffect(() => {
     const getData = async () => {
-      const pokemon = await fetchAllPokemon();
-      const { results } = pokemon;
-      setPokemonList(results);
+      const allPokemon = await fetchAllPokemon();
+      const { results } = allPokemon;
+      const arrayPokemon = await Promise.all(results.map(async (result) => {
+        const eachPokemon = await fetchPokemon(result.name);
+        const { dream_world: { front_default }} = eachPokemon.sprites.other;
+        return {
+          ...result,
+          sprite: front_default,
+        }
+      }));
+      console.log(arrayPokemon);
+      setPokemonList(arrayPokemon);
     };
   
     getData();
@@ -16,10 +27,12 @@ export default function List() {
 
   console.log(pokemonList);
   return (
-    <div>
-      {pokemonList.map((pokemon) => (
-        <div key={pokemon.name}>
+    <div className="hero-list">
+      {pokemonList?.map((pokemon) => (
+        <div className="pokemon-card" key={pokemon.name}>
+          <img className="pokemon-images" src={pokemon.sprite} alt={pokemon.name} />
           <h1>{pokemon.name}</h1>
+          <button><img src={ favoriteIcon } alt="Botão de favoritar"/>Favoritar</button>
         </div>
       ))}
     </div>
