e7466446f63380bdf6cb413f4c506a2f3dc3d7e8
Joblanco88 <jo_blanco9@yahoo.com.br>
2023-04-05 18:18:13 -0300

    Configurado para utilizar o context

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { fetchAllPokemon, fetchPokemon } from '../helpers/api';
 import blackHeart from '../images/blackHeartIcon.svg';
 import { getLocalStorage, saveLocalStorage } from '../helpers/storage';
+import Context from '../context/Context';
 
 export default function List() {
   const [pokemonList, setPokemonList] = useState([]);
-  const [favoritesPokemon, setFavoritesPokemon] = useState({ favorites: []});
+  const { globalState, setGlobalState } = useContext(Context);
   const favoriteIcon = blackHeart;
 
   useEffect(() => {
     const localStorage = getLocalStorage('favorites');
     if (!localStorage) saveLocalStorage('favorites', []);
-    setFavoritesPokemon({ favorites: localStorage});
+    setGlobalState({ favorites: localStorage});
     const getData = async () => {
       const allPokemon = await fetchAllPokemon();
       const { results } = allPokemon;
@@ -36,12 +37,12 @@ export default function List() {
       if (boolean) {
         const array = localStorage.filter((pokemon) => pokemon.name !== value);
         saveLocalStorage('favorites', array);
-        setFavoritesPokemon({ favorites: array});
+        setGlobalState({ favorites: array});
         global.alert('Favorito removido com sucesso!');
        } else { 
         const array = [...pokemonList.filter((pokemon) => pokemon.name === value), ...localStorage]
         saveLocalStorage('favorites', array);
-        setFavoritesPokemon({ favorites: array});
+        setGlobalState({ favorites: array});
         global.alert('Favorito salvo com sucesso!');
       }
     } else {
@@ -51,17 +52,16 @@ export default function List() {
       if (boolean) {
         const array = localStorage.filter((pokemon) => pokemon.name !== pokeAlt);
         saveLocalStorage('favorites', array);
-        setFavoritesPokemon({ favorites: array});
+        setGlobalState({ favorites: array});
         global.alert('Favorito removido com sucesso!');
       } else {
         const array = [...pokemonList.filter((pokemon) => pokemon.name === pokeAlt), ...localStorage];
         saveLocalStorage('favorites', array);
-        setFavoritesPokemon({ favorites: array});
+        setGlobalState({ favorites: array});
         global.alert('Favorito salvo com sucesso!');
       }
     }
   }
-  console.log(favoritesPokemon);
   return (
     <div className="hero-list">
       <header className='header-list'>Lista</header>
@@ -73,7 +73,7 @@ export default function List() {
         </div>
       ))}
       <div className="favorites-list">
-        {favoritesPokemon.favorites?.map((pokemon) => (
+        {globalState.favorites?.map((pokemon) => (
           <div className="pokemon-card" key={pokemon.name}>
             <img className="pokemon-images" src={pokemon.sprite} alt={pokemon.name} />
             <h1>{pokemon.name}</h1>
