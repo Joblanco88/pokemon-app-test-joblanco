49320b173841bc2d79271f8c06a94cc27dc26144
Joblanco88 <jo_blanco9@yahoo.com.br>
2023-04-05 01:39:55 -0300

    Adicionado css e elementos à page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import pokemonLogo from '../images/pokemonLogo.png';
 
 export default function Home() {
+  const appDescription = "Esse app tem como objetivo renderizar uma lista de pokémon e poder interagir com ela, adicionando cada pokémon aos seus favoritos ou removê-los da sua lista."
   return (
-    <div>
-      <header>
-        <span>Home</span>
-      </header>
-      <h1>Bem Vindo ao App Teste!</h1>
-      <img src="" alt="" />
-      <h3>Esse app tem como objetivo renderizar uma lista de pokémon e poder interagir com ela, adicionando cada pokémon aos seus favoritos ou removê-los da sua lista.</h3>
+    <div className="hero-home">
+
+      <header className='header-main'>Home</header>
+
+      <section className="welcome-home">
+        <h1 id="welcomeHeading">Bem Vindo ao App Teste com tema do Pokémon!</h1>
+        <img id="pokemonLogo" src={ pokemonLogo } alt="logo pokémon" />
+        <h3 id="appDescription">{ appDescription }</h3>
+      </section>
+
+      <section className="button-home">
+        <span>Clique no botão para começar sua experiência!</span>
+        <button type="button">Pokémon</button>
+      </section>
 
     </div>
   );
