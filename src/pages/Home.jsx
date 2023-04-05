01b98315596daff9284c5018afa83c6e386400d0
Joblanco88 <jo_blanco9@yahoo.com.br>
2023-04-05 14:57:29 -0300

    Adicionado link como segunda opcao

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import pokemonLogo from '../images/pokemonLogo.png';
 import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   const appDescription = "Esse app tem como objetivo renderizar uma lista de pokémon e poder interagir com ela, adicionando cada pokémon aos seus favoritos ou removê-los da sua lista."
@@ -17,8 +18,9 @@ export default function Home() {
       </section>
 
       <section className="button-home">
-        <span>Clique no botão para começar sua experiência!</span>
+        <span>Clique no botão ou no Link para começar sua experiência!</span>
         <button type="button" onClick={ () => history.push('/list') }>Pokémon</button>
+        <Link to="/list">Pokémon</Link>
       </section>
 
     </div>
