3160039ffd4065cc0b4e155c11ccac63d5f784cb
Joblanco88 <jo_blanco9@yahoo.com.br>
2023-04-05 18:13:34 -0300

    Adicionado função de adicionar e remover ao click do botão, renderização dinâmica dos favoritos

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -5,11 +5,13 @@ import { getLocalStorage, saveLocalStorage } from '../helpers/storage';
 
 export default function List() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [favoritesPokemon, setFavoritesPokemon] = useState({ favorites: []});
   const favoriteIcon = blackHeart;
 
   useEffect(() => {
     const localStorage = getLocalStorage('favorites');
     if (!localStorage) saveLocalStorage('favorites', []);
+    setFavoritesPokemon({ favorites: localStorage});
     const getData = async () => {
       const allPokemon = await fetchAllPokemon();
       const { results } = allPokemon;
@@ -27,21 +29,39 @@ export default function List() {
     getData();
   }, []);
 
-  const saveFavorites = ({target: {value, alt}}) => {
+  const setFavorites = ({target: {value, alt}}) => {
     const localStorage = getLocalStorage('favorites');
-
     if (value) {
-      pokemonList.filter((pokemon) => pokemon === value);
-      saveLocalStorage('favorites', [...pokemonList.filter((pokemon) => pokemon.name === value), ...localStorage]);
-      global.alert('Local Storage salvo com sucesso!');
+      const boolean = localStorage.some((pokemon) => pokemon.name === value);
+      if (boolean) {
+        const array = localStorage.filter((pokemon) => pokemon.name !== value);
+        saveLocalStorage('favorites', array);
+        setFavoritesPokemon({ favorites: array});
+        global.alert('Favorito removido com sucesso!');
+       } else { 
+        const array = [...pokemonList.filter((pokemon) => pokemon.name === value), ...localStorage]
+        saveLocalStorage('favorites', array);
+        setFavoritesPokemon({ favorites: array});
+        global.alert('Favorito salvo com sucesso!');
+      }
     } else {
       const pokemonName = alt.split(' ');
       const pokeAlt = pokemonName.pop();
-      saveLocalStorage('favorites', [...pokemonList.filter((pokemon) => pokemon.name === pokeAlt), ...localStorage]);
-      global.alert('Local Storage salvo com sucesso!');
+      const boolean = localStorage.some((pokemon) => pokemon.name === pokeAlt);
+      if (boolean) {
+        const array = localStorage.filter((pokemon) => pokemon.name !== pokeAlt);
+        saveLocalStorage('favorites', array);
+        setFavoritesPokemon({ favorites: array});
+        global.alert('Favorito removido com sucesso!');
+      } else {
+        const array = [...pokemonList.filter((pokemon) => pokemon.name === pokeAlt), ...localStorage];
+        saveLocalStorage('favorites', array);
+        setFavoritesPokemon({ favorites: array});
+        global.alert('Favorito salvo com sucesso!');
+      }
     }
   }
-
+  console.log(favoritesPokemon);
   return (
     <div className="hero-list">
       <header className='header-list'>Lista</header>
@@ -49,11 +69,17 @@ export default function List() {
         <div className="pokemon-card" key={pokemon.name}>
           <img className="pokemon-images" src={pokemon.sprite} alt={pokemon.name} />
           <h1>{pokemon.name}</h1>
-          <button value={pokemon.name} onClick={ saveFavorites }><img src={ favoriteIcon } alt={`Botão de favoritar do ${pokemon.name}`}/>Favoritar</button>
+          <button value={pokemon.name} onClick={ setFavorites }><img src={ favoriteIcon } alt={`Botão de favoritar do ${pokemon.name}`}/>Favoritar</button>
         </div>
       ))}
       <div className="favorites-list">
-        Favoritos
+        {favoritesPokemon.favorites?.map((pokemon) => (
+          <div className="pokemon-card" key={pokemon.name}>
+            <img className="pokemon-images" src={pokemon.sprite} alt={pokemon.name} />
+            <h1>{pokemon.name}</h1>
+            <button value={pokemon.name} onClick={ setFavorites }><img src={ favoriteIcon } alt={`Botão de favoritar do ${pokemon.name}`}/>Favoritar</button>
+          </div>
+        ))}
       </div>
     </div>
   );
