bcd6132d60eec53fa0889d25c728f986460f22d1
Joblanco88 <jo_blanco9@yahoo.com.br>
2023-04-06 13:47:36 -0300

    Adicionado helper para os testes e o primeiro teste

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import App from './App';
+import { renderWithRouter } from './helpers/renderWith';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('renders learn react link', () => {
+  test('Verifica se existe a imagem correta e o link com redirecionamento correto', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkElement = screen.getByRole('link', { name: 'Pokémon' });
+    const imagePoke = screen.getByRole('img', { alt: 'logo pokémon' });
+    expect(linkElement).toBeInTheDocument();
+    expect(imagePoke).toBeInTheDocument();
+    expect(imagePoke).toHaveAttribute('src', 'pokemonLogo.png');
+
+    fireEvent.click(linkElement);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/list');
+
+  });
 });
