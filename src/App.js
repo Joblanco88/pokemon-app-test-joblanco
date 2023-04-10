68da17ffa0459e6b84175142f255f78a7a137142
Joblanco88 <jo_blanco9@yahoo.com.br>
2023-04-10 00:34:05 -0300

    Removido css sem uso

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
-// import Home from './pages/Home';
-// import List from './pages/List';
 import Home from '@/pages/Home';
 import List from '@/pages/List';
 import Provider from '@/context/Provider';
